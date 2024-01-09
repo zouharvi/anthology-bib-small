3c692f619447af61fc0a5c38aed33620990de59e
Vilém Zouhar <vilem.zouhar@gmail.com>
2024-01-09 11:34:47 +0000

    init commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# `anthology.bib` minified
+
+A minified version of the official [aclanthology.bib](https://aclanthology.org/) without _adress_, _editor_, _month_, and duplicate _url+doi_ fields.
+
+Get it here: [TODO](TOOD) or:
+```
+curl TODO > anthology.min.bib
+```
+
+
+## Size difference
+
+```bash
+> ls -lh data/
+58M anthology.bib
+41M anthology.min.bib
+```
---
+++ minify.py
@@ -0,0 +1,17 @@
+from pybtex.database import parse_file
+bib_data = parse_file('data/anthology.bib')
+
+for entry in bib_data.entries:
+    if "address" in bib_data.entries[entry].fields:
+        bib_data.entries[entry].fields.pop("address")
+    if "month" in bib_data.entries[entry].fields:
+        bib_data.entries[entry].fields.pop("month")
+
+    if "url" in bib_data.entries[entry].fields and "doi" in bib_data.entries[entry].fields:
+        bib_data.entries[entry].fields.pop("url")
+
+    if "editor" in bib_data.entries[entry].persons:
+        bib_data.entries[entry].persons.pop("editor")
+
+
+bib_data.to_file("anthology.min.bib", bib_format="bibtex")
